6da0d972e54d78932ae4c9f090ec74d51364f5a9
ShawSong <songshawyx@163.com>
2017-08-07 11:44:00 +0800

    Update:韦俊俊,代码示例。

---
+++ WeiJJ/java/test.java
@@ -0,0 +1,23 @@
+package weaver.cpt.util;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import weaver.conn.RecordSet;
+import weaver.cpt.util.CptFieldManager;
+import weaver.general.BaseBean;
+import weaver.general.TimeUtil;
+import weaver.hrm.User;
+import weaver.systeminfo.SystemEnv;
+
+public class CptDetailColumnUtilNew extends BaseBean {
+    private RecordSet rs = new RecordSet();
+    private CptFieldManager cptFieldManager = new CptFieldManager();
+
+    public CptDetailColumnUtilNew() {
+    }
+
+    public JSONArray getDetailColumnConf(String var1, User var2) {
+        
+        return var3;
+    }
+}
